4a63675076593cba00b3d9af3d223cd77eae0757
Kira.Pham <kira.pham@pal.net.vn>
2026-01-06 16:16:24 +0700

    init module tool-service

---
+++ kira-tool-service/src/main/java/app/auto/be/kiratoolservice/KiraToolServiceApplication.java
@@ -0,0 +1,13 @@
+package app.auto.be.kiratoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiraToolServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiraToolServiceApplication.class, args);
+    }
+
+}
